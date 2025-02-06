6d53241de6cce2b79f0b51dc739369a9314054a8
mohitsing_chaudhari <mohitsingsc@gmail.com>
2025-02-07 00:06:38 +0530

    exploring get post patch delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from test")
-})
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Mohit",gfName:"Vaishnavi"});
+});
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hello");
-})
+app.post("/user",(req,res)=>{
+    res.send("Data posted successfully");
+});
 
-app.use("/",(req,res)=>{
-    res.send("Jay shri ram");
-})
+app.put("/user",(req,res)=>{
+    res.send("Data putted successfully");
+});
+
+app.patch("/user",(req,res)=>{
+    res.send("Data patched successfully");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted successfully");
+});
 
 app.listen(7777);
