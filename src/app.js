944fed2fe45a9e999ee0b7494b282d81936dd1b6
mohitsing_chaudhari <mohitsingsc@gmail.com>
2025-02-12 00:37:13 +0530

    get update delete user operation

--- src/app.js
+++ src/app.js
@@ -8,16 +8,72 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+
+//sign up API
 app.post("/signup",async(req,res)=>{
     const user = new User(req.body);
     try{
         await user.save();
         res.send("User added successfully");
     }catch(err){
+        console.log(err.message);
         res.status(400).send("Unable to save user");
     }
 });
     
+//get one user with specific email
+app.get("/user",async(req,res)=>{
+    try{
+        const userEmail = req.body.email;
+        const user = await User.find({email : userEmail});
+
+        if(!user){
+            res.send("User not found");
+        }else{
+            res.send(user);
+        }
+    }catch(err){
+        console.log(err.message);
+        res.send("Something went wrong");
+    }
+});
+
+
+// feed api will give you all users
+app.get("/feed",async (req,res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+        res.send("Something went wrong");
+    }
+});
+
+
+// deleting a user
+app.delete("/user",async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const deletedUser = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }catch(err){
+        res.send("Something went wrong");
+    }
+});
+
+
+//updating user data
+app.patch("/user",async(req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+        await User.findByIdAndUpdate(userId,data);
+        res.send("User updated successfully");
+    }catch(err){
+        res.send("Something went wrong");
+    }
+})
+
 
 
 connectDB().then(()=>{
@@ -25,7 +81,7 @@ connectDB().then(()=>{
     app.listen(7777,()=>{
         console.log("Server is successfully listening on port 7777");
     });
-}).catch(()=>{
-    console.error("Database not connected");
+}).catch((err)=>{
+    console.error("Database not connected",err.message);
 });
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async ()=>{
-    await mongoose.connect("mongodb+srv://mohitsingsc:mohitsing2005@vmchaudhari.jdgf0.mongodb.net/devTinder");
+    await mongoose.connect("mongodb+srv://mohitsingsc:o3PuRH97DpoEERdx@vmchaudhari.jdgf0.mongodb.net/devTinder");
 }
 
 module.exports = {
